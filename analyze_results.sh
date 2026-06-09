ff58c89e658e54840f3e7cca7cc213209281e459
Chobyeongcheon <164847365+chojo-00@users.noreply.github.com>
2026-06-09 12:24:48 +0900

    Add files via upload

--- analyze_results.sh
+++ analyze_results.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
-#SBATCH -J mtd_gan_train_ver2_denoising_30
+#SBATCH -J mtd_gan_analyze_results_ver2
 #SBATCH -t 7-00:00:00
 #SBATCH -o logs/%x_%A_%N.out
-#SBATCH --mail-type END,TIME_LIMIT_90,REQUEUE,INVALID_DEPEND,BEGIN
+#SBATCH --mail-type BEGIN,END
 #SBATCH --mail-user chobyeongcheon00@gmail.com
-#SBATCH -p RTX3090
-#SBATCH -w gpu31
+#SBATCH -p TitanRTX
+#SBATCH -w gpu14
 #SBATCH --gres=gpu:1
-
 
 export HTTP_PROXY="http://192.168.45.108:3128"
 export HTTPS_PROXY="http://192.168.45.108:3128"
 
 # Define vars
-JOB_NAME="mtd_gan_train_ver2_denoising_30"
+JOB_NAME="mtd_gan_analyze_results_ver2"
 DOCKER_IMAGE_NAME="bc_cho/${JOB_NAME}"
 DOCKER_CONTAINER_NAME="bc_cho${JOB_NAME}"
-PORT_NUM=4999
-
-
+PORT_NUM=4145
 
 # Paths inside the container
 CODE_DIR="/workspace/bc_cho/0_Project/2_LDCT2NDCT/MTD-GAN_ver2"
-CHECKPOINT_DIR="${CODE_DIR}/checkpoints/denoising_30"
-SAVE_DIR="${CODE_DIR}/predictions/denoising_30"
+CHECKPOINT_DIR="${CODE_DIR}/checkpoints/Quarter_60to30"
+# 테스트 결과 폴더 (pred_results.csv 가 있는 경로)
+SAVE_DIR="${CODE_DIR}/test_results/Quarter_60to30" 
+# 분석 결과가 섞이지 않도록 하위 폴더 'analysis' 지정
+ANALYSIS_DIR="${SAVE_DIR}/ssim_analysis"
 
-
-# Run containers
+# Build container
 docker build -t ${DOCKER_IMAGE_NAME} -f Dockerfile .
 
 # Stop running container
@@ -41,8 +40,6 @@ if docker ps -a -q --filter "name=${DOCKER_CONTAINER_NAME}" | grep -q .; then
     docker rm ${DOCKER_CONTAINER_NAME}
 fi
 
-
-2
 # Run containers
 docker run --rm \
         --name ${DOCKER_CONTAINER_NAME} \
@@ -54,27 +51,14 @@ docker run --rm \
         ${DOCKER_IMAGE_NAME} \
         bash -c "
             cd ${CODE_DIR} && \
-            python3 train.py \
-                --dataset my_chest_data \
-                --dataset-type-train 'full_patch' \
-                --dataset-type-valid 'full' \
-                --in-kernel 'B30f' \
-                --task 'denoising' \
-                --batch-size 48 \
-                --train-num-workers 16 \
-                --valid-num-workers 16 \
+            python3 analyze_results.py \
+                --csv ${SAVE_DIR}/pred_results.csv \
+                --save-dir ${ANALYSIS_DIR} \
+                --in-kernel 'B60f' \
+                --gt-kernel 'B30f' \
                 --model 'MTD_GAN_Method' \
-                --loss 'L1 Loss' \
-                --method 'pcgrad' \
-                --optimizer 'adamw' \
-                --scheduler 'poly_lr' \
-                --epochs 10 \
-                --warmup-epochs 0 \
-                --multi-gpu-mode Single \
-                --lr 5e-4 \
-                --min-lr 1e-5 \
-                --print-freq 10 \
-                --save-checkpoint-every 1 \
-                --checkpoint-dir ${CHECKPOINT_DIR} \
-                --save-dir ${SAVE_DIR} 
+                --resume ${CHECKPOINT_DIR}/epoch_9_checkpoint.pth \
+                --rank-metric 'SSIM' \
+                --unique-patient \
+                --k 10
         "
--- test_ver2.sh
+++ test_ver2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -J mtd_gan_test_ver2_full_60to30
+#SBATCH -J mtd_gan_test_ver2_denoising_60
 #SBATCH -t 7-00:00:00
 #SBATCH -o logs/%x_%A_%N.out
 #SBATCH --mail-type BEGIN,END
 #SBATCH --mail-user chobyeongcheon00@gmail.com
 #SBATCH -p TitanRTX
-#SBATCH -w gpu15
+#SBATCH -w gpu17
 #SBATCH --gres=gpu:1
 
 
@@ -13,18 +13,18 @@ export HTTP_PROXY="http://192.168.45.108:3128"
 export HTTPS_PROXY="http://192.168.45.108:3128"
 
 # Define vars
-JOB_NAME="mtd_gan_test_ver2_full_60to30"
+JOB_NAME="mtd_gan_test_ver2_denoising_60"
 DOCKER_IMAGE_NAME="bc_cho/${JOB_NAME}"
 DOCKER_CONTAINER_NAME="bc_cho${JOB_NAME}"
-PORT_NUM=4140
+PORT_NUM=4142
 
 
 
 # Paths inside the container
 CODE_DIR="/workspace/bc_cho/0_Project/2_LDCT2NDCT/MTD-GAN_ver2"
-CHECKPOINT_DIR="${CODE_DIR}/checkpoints/Full_60to30"
+CHECKPOINT_DIR="${CODE_DIR}/checkpoints/denoising_60"
 # 테스트 결과 이미지가 섞이지 않도록 별도의 폴더 지정
-SAVE_DIR="${CODE_DIR}/test_results/Full_60to30" 
+SAVE_DIR="${CODE_DIR}/test_results/denoising_60" 
 
 
 # Run containers
@@ -58,8 +58,7 @@ docker run --rm \
                 --dataset my_chest_data \
                 --dataset-type-test 'full' \
                 --in-kernel 'B60f' \
-                --gt-kernel 'B30f' \
-                --dose 'full' \
+                --task 'denoising' \
                 --test-batch-size 1 \
                 --test-num-workers 16 \
                 --model 'MTD_GAN_Method' \
@@ -71,3 +70,8 @@ docker run --rm \
                 --save-dir ${SAVE_DIR} \
                 --epoch 10
         "
+
+
+## parser 모음
+##                 --gt-kernel 'B60f' \
+##                 --dose 'full' \
